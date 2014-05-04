c51ff325277bcaacad987170d5fc80decd30ad25
Kamil Jiwa <kamil.jiwa@gmail.com>
2014-05-04 01:17:57 -0700

    Make lint happy

--- template.py
+++ template.py
@@ -3,14 +3,14 @@
 import jinja2
 
 def render(filename):
-  """Render an HTML template as a string.
+    """Render an HTML template as a string.
 
-  Args:
-    filename: The HTML template file name.
+    Args:
+      filename: The HTML template file name.
 
-  Returns:
-    A string of the rendered HTML.
-  """
-  with open(filename) as f:
-    template = jinja2.Template(f.read())
-    return template.render()
+    Returns:
+      A string of the rendered HTML.
+    """
+    with open(filename) as handle:
+        template = jinja2.Template(handle.read())
+        return template.render()
