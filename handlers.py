c2c659afa879217c9176cab7420645358e7f3f60
Kamil Jiwa <kamil.jiwa@gmail.com>
2014-05-03 18:51:05 -0700

    Use jinja2 templates

--- handlers.py
+++ handlers.py
@@ -1,16 +1,21 @@
 """URL handlers"""
 
+import jinja2
+import os
 import webapp2
 
 class Index(webapp2.RequestHandler):
     """Index request handler"""
     def get(self):
         """GET method handler"""
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello')
+        with open(os.getcwd() + '/www/index.html') as f:
+            template = jinja2.Template(f.read())
+
+        self.response.charset = 'utf8'
+        self.response.text = template.render()
 
 class Ping(webapp2.RequestHandler):
     """Ping request handler"""
     def get(self):
         """GET method handler"""
-        pass
+        self.response.status = 204
